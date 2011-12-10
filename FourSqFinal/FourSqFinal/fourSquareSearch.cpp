074c241e56de33b4c0e96f7792aeb9237fc8e31b
hitoyozake <mikoto_1122@yahoo.co.jp>
2011-12-10 17:41:32 +0900

    ptree ver

--- FourSqFinal/FourSqFinal/fourSquareSearch.cpp
+++ FourSqFinal/FourSqFinal/fourSquareSearch.cpp
@@ -2,12 +2,18 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include <codecvt>
 
 #include <boost/asio.hpp>
 #include <boost/optional.hpp>
 #include <boost/algorithm/string/replace.hpp>
+#include <boost/io/ios_state.hpp>
+#include <boost/progress.hpp>
 
 #include <boost/asio/ssl.hpp>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 //icuuc
 #include <Windows.h>
@@ -17,7 +23,9 @@
 
 namespace search
 {
-	boost::optional< std::string > search_ssl( const std::string & url )
+	std::map< std::string, std::string > mp;
+	std::string key;
+	boost::optional< std::string > search_ssl( const std::string & url, boost::property_tree::ptree & log )
 	{
 		try
 		{
@@ -70,25 +78,43 @@ namespace search
 			}
 
 			const auto e_mes = error.message();
-
-			const auto venue = log_str.find( "options['venue']" );
+			const std::string venuestr = "options['venue']";
+			const auto venue = log_str.find( venuestr );
 			const auto checkin = log_str.find( "options['checkinId']" );
 			//std::cout << log_str;
 			if(  venue != std::string::npos && checkin != std::string::npos )
 			{
-				auto tmp_str = log_str.substr( venue, checkin - venue - 1 );
+				std::string tmp_str = log_str.substr( venue + venuestr.size() + 2, checkin - ( venue + venuestr.size() + 2 ) - 2 );
 				boost::algorithm::replace_all( tmp_str, "\n", "" );
 				boost::algorithm::replace_all( tmp_str, "\t", "" );
 				boost::algorithm::replace_all( tmp_str, " ", "" );
 				boost::algorithm::replace_all( tmp_str, "\\", "" );//エスケープ文字削除
 
+				tmp_str.pop_back();
+				//tmp_str.pop_back();
 
-				std::cout << tmp_str << std::endl;
+				//tmp_str = tmp_str.substr( 1 );
+
+				boost::property_tree::ptree p;
+
+				std::stringstream s;
+				s << tmp_str;
+
+				std::cout << s.str();
+
+				std::cout << s << std::endl;
+
+				boost::property_tree::json_parser::read_json( s, p );
+
+				log.push_back( std::make_pair( "", p ) );
+
+				mp.insert( std::map< std::string, std::string >::value_type( key, tmp_str ) );
 			}
 			return boost::optional< std::string >();
 		}
 		catch( std::exception & e )
 		{
+			std::cerr << e.what() << std::endl;
 			return boost::optional< std::string >();
 		}
 	}
@@ -129,6 +155,7 @@ namespace search
 				result += line+ "\n";
 				//std::cout << line << std::endl;
 			}
+
 			return boost::optional< std::string >( result );
 		}
 		catch( std::exception & e )
@@ -138,74 +165,193 @@ namespace search
 		}
 	}
 }
-int main()
+
+void search_main()
 {
 	using namespace search;
-	std::ifstream ifs( "input.txt" );
-	std::ofstream ofs( "output2.txt" );
+	using namespace boost::property_tree;
 
-	std::cout.rdbuf( ofs.rdbuf() );
+	boost::io::ios_rdbuf_saver saver( std::cout );
+
+	std::ifstream ifs( "input.txt" );
+
+	enum STATE
+	{
+		E_NULL,
+		E_SEARCH_START,
+		E_SEARCH_USER_NAME,
+		E_SEARCH_FIRST_DATE,
+		E_SEARCH_DATE,
+		E_SEARCH_DATA_OR_DATE_OR_END,
+		E_END
+	};
+
+	int state = E_SEARCH_START;
+
+	wptree json;
+	ptree users;
+	ptree days;
+	ptree logs;
+	ptree user;
+	ptree day;
+
+
 
 	while( ! ifs.eof() )
 	{
 		std::string str;
+
 		std::getline( ifs, str );
 
-		if( str.find( "http://" ) != std::string::npos )
+		boost::algorithm::replace_all( str, "\t", "" );
+
+		if( state == E_END || state == E_SEARCH_START )
 		{
-			if( str.find( "http://t.co" ) != std::string::npos )
+			if( str.find( "start" ) == 0 )
 			{
-				const auto tco_url = str.substr( str.find( "http" ), str.find( "{" ) - str.find( "http" ) - 1 );
-				const auto tco = search_http( tco_url );
+				state = E_SEARCH_USER_NAME;
+				user = ptree();
+				days = ptree();
+				day = ptree();
+				logs = ptree();
 
-				if( tco )
+				continue;
+			}
+		}
+
+		if( state == E_SEARCH_USER_NAME )
+		{
+			//ユーザ名を記録
+			user.put( "user", str );
+			state = E_SEARCH_FIRST_DATE;
+			continue;
+		}
+
+		if( state == E_SEARCH_FIRST_DATE )
+		{
+			if( str.find("d") == 0 )
+			{
+				//日付だった
+				day.put( "day", str.substr( 1 ) );
+				state = E_SEARCH_DATA_OR_DATE_OR_END;
+				continue;
+			}
+		}
+
+		if( state == E_SEARCH_DATA_OR_DATE_OR_END || state == E_SEARCH_DATA_OR_DATE_OR_END )
+		{
+			if( str.find("d") == 0 )
+			{
+				//日付だった
+				day.add_child( "logs", logs );
+				days.push_back( std::make_pair( "", day ) );
+				day = ptree();
+				day.put( "day", str.substr( 1 ) );
+				continue;
+			}
+
+			if( str.find( "http://" ) != std::string::npos )
+			{
+				//key検索
+				const auto imatf = str.find("http://" );
+
+				const auto imat = str.substr( imatf );
+
+				if( imatf != std::string::npos )
 				{
-					const auto http_index = tco->find( "http" );
-					if( http_index == std::string::npos )
-						continue;
-					const auto url_4sq = tco->substr( http_index, tco->find( "Cache" ) - http_index - 2 );
-					//4sq抽出 -2は\n切り
+					const auto itr = mp.find( imat );
 
-					const auto fsq = search_http( url_4sq );
-
-					if( fsq )
+					if( itr != mp.end() )
 					{
-						const auto s = * fsq;
-						//リダイレクト処理
-						if( fsq->find( "href=\"https://foursquare" ) != std::string::npos )
+						//見つかった
+						ptree log;
+						log.put( "info", itr->second );
+						logs.push_back( std::make_pair( "", log ) );
+						continue;
+					}
+				}
+				//新規場所なのでキーに記憶
+				key = imat;
+				//サイトだった
+				if( str.find( "http://t.co" ) != std::string::npos )
+				{
+					const auto tco_url = str.substr( str.find( "http" ), str.find( "{" ) - str.find( "http" ) - 1 );
+					const auto tco = search_http( tco_url );
+
+					if( tco )
+					{
+						const auto http_index = tco->find( "http" );
+						if( http_index == std::string::npos )
+							continue;
+						const auto url_4sq = tco->substr( http_index, tco->find( "Cache" ) - http_index - 2 );
+						//4sq抽出 -2は\n切り
+
+						const auto fsq = search_http( url_4sq );
+
+						if( fsq )
 						{
-							const auto hrefsub = fsq->substr( fsq->find( "href=\"https://foursquare" ) );
-							const auto href_index = fsq->find( "\">" );
-							const auto len = hrefsub.find( "\">" );
-							const auto url_fsq = hrefsub.substr( 14, len - 14 );
-							search_ssl( url_fsq );
-							//sh.search_ssl( url_fsq );
+							const auto s = * fsq;
+							//リダイレクト処理
+							if( fsq->find( "href=\"https://foursquare" ) != std::string::npos )
+							{
+								ptree log;
+								const auto hrefsub = fsq->substr( fsq->find( "href=\"https://foursquare" ) );
+								const auto href_index = fsq->find( "\">" );
+								const auto len = hrefsub.find( "\">" );
+								const auto url_fsq = hrefsub.substr( 14, len - 14 );
+								search_ssl( url_fsq, log );
+
+								logs.push_back( std::make_pair( "", log ) );	
+							}
 						}
 					}
 				}
-			}
-			if( str.find( "http://4sq" ) != std::string::npos )
-			{
-				const auto fsu = str.substr( str.find( "http" ) );
-				const auto fsq_url = fsu.substr( 0, fsu.find( " " ) );
-				const auto fsq = search_http( fsq_url );
-
-				if( fsq )
+				if( str.find( "http://4sq" ) != std::string::npos )
 				{
-					const auto fsq_href_index = fsq->find( "href=" );
-					//リダイレクト処理
-					if( fsq->find( "href=" ) != std::string::npos )
+					const auto fsu = str.substr( str.find( "http" ) );
+					const auto fsq_url = fsu.substr( 0, fsu.find( " " ) );
+					const auto fsq = search_http( fsq_url );
+
+					if( fsq )
 					{
-						const auto hrefsub = fsq->substr( fsq->find( "href=" ) );
-						const auto href_index = fsq->find( "\">" );
-						const auto len = hrefsub.find( "\">" );
-						const auto url_fsq = hrefsub.substr( 14, len - 14 );
-						search_ssl( url_fsq );
+						const auto fsq_href_index = fsq->find( "href=" );
+						//リダイレクト処理
+						if( fsq->find( "href=" ) != std::string::npos )
+						{
+							ptree log;
+							const auto hrefsub = fsq->substr( fsq->find( "href=" ) );
+							const auto href_index = fsq->find( "\">" );
+							const auto len = hrefsub.find( "\">" );
+							const auto url_fsq = hrefsub.substr( 14, len - 14 );
+							search_ssl( url_fsq, log );
+							logs.push_back( std::make_pair( "", log ) );					
+
+						}
 					}
+
+					continue;
 				}
 			}
+
+		}
+		if( str.find( "end" ) == 0 )
+		{
+			user.add_child( "day", days );
+			users.push_back( std::make_pair( "", user ) );
+
+			state = E_END;
+			continue;
 		}
 	}
+	json.add_child( L"users", users );
+	write_json( "output.json", json );
+}
 
+
+
+int main()
+{
+	boost::progress_timer t;
+	search_main();
 	return 0;
 }
--- FourSqFinal/JSONReader/json.cpp
+++ FourSqFinal/JSONReader/json.cpp
@@ -3,26 +3,57 @@
 #include <boost/optional.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <fstream>
 
-int main()
+void reader()
 {
 	using namespace boost::property_tree;
 
 	ptree pt;
-	
+
 	try
 	{
-		json_parser::read_json( "output2.txt", pt );
+		json_parser::read_json( "output.txt", pt );
 	}
 	catch( std::exception & e )
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
-	if( const auto result = pt.get_optional< std::string >( "venue.id" ) )
+	if( const auto result = pt.get_optional< std::string >( "“ú–{Œê" ) )
 	{
 		std::cout << result.get() << std::endl;
 	}
+}
 
+void writer()
+{
+	using namespace boost::property_tree;
+
+	wptree pt;
+	wptree child;
+
+	pt.put( L"‚ ‚¢‚¤‚¦‚¨“ú–{Œê\"", L"–kŠC“¹—JŸT" );
+
+	//std::cout << pt.get< std::string >( "\"‚ ‚¢‚¤‚¦‚¨“ú–{Œê" ) << std::endl;
+
+	try
+	{
+		write_json( "ts.json", pt );
+	}
+	catch( std::exception & e )
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+}
+
+
+int main()
+{
+	
+	writer();
+	reader();
 	return 0;
 }
